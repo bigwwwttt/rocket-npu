790e7a4103d5fe614efe4cb82146cca80b3e9e21
bigwwwttt <1048333431@qq.com>
2024-12-24 20:39:02 +0800

    first commit

---
+++ build.sbt
@@ -0,0 +1,4 @@
+organization := "edu.berkeley.cs"
+version := "3.0"
+name := "rocketchipnpu"
+scalaVersion := "2.13.10"
---
+++ src/main/scala/rocket/package.scala
@@ -0,0 +1,5 @@
+// See LICENSE.Berkeley for license details.
+
+package rocketchipnpu
+
+package object rocket extends rocket.constants.ScalarOpConstants with rocket.constants.MemoryOpConstants
